e8fae678714ef5ea29b1e78f8502c05a315a3102
Hoang Trung Anh <anh.hoang04k22@hcmut.edu.vn>
2024-04-01 15:54:26 +0700

    create header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,42 @@
+import { Link } from 'react-router-dom';
 import clsx from 'clsx';
 import style from './Header.module.scss';
+import Logo from '~/img/logo.png';
 
 function Header() {
     return (
-        <header>
-            <nav className={clsx(style.nav, style.nav__active)}>
-                <h1>Header</h1>
+        <header id={clsx(style.header)}>
+            <div className={clsx(style.logo)}>
+                <img src={Logo} alt="logo" />
+            </div>
+            <nav className={clsx(style.nav)}>
+                <ul>
+                    <li className={clsx(style.active)}>
+                        <Link to="/dashboard/admin">Dashboard</Link>
+                    </li>
+                    <li>
+                        <Link to="/staffs">Staffs</Link>
+                    </li>
+                    <li>
+                        <Link to="/patients">Patients</Link>
+                    </li>
+                    <li>
+                        <Link to="/medicine">Medicine</Link>
+                    </li>
+                    <li>
+                        <Link to="/equipment">Equipment</Link>
+                    </li>
+                </ul>
             </nav>
+            <div className={clsx(style.user)}>
+                <div className={clsx(style.infor)}>
+                    <div className={clsx(style.name)}>This is a name</div>
+                    <div>Admin</div>
+                </div>
+                <div className={clsx(style.img)}>
+                    <img src={Logo} alt="" />
+                </div>
+            </div>
         </header>
     );
 }
