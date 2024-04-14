4e5b3c8f5cd6eee1e81350bb6a74274160910fd2
ThanhKhanh <nguyenthanhkhanh2304@gmail.com>
2024-04-15 00:46:56 +0700

    update page medicine and connect to firestore and storage

--- src/pages/Medicine/index.js
+++ src/pages/Medicine/index.js
@@ -2,18 +2,42 @@ import clsx from 'clsx';
 import styles from "./Medicine.module.scss";
 import SearchIcon from '@mui/icons-material/Search';
 import MedicationOutlinedIcon from '@mui/icons-material/MedicationOutlined';
-import { useState } from 'react';
-import Medicines from './medicine_data';
+import { useState, useEffect } from 'react';
 import ScienceOutlinedIcon from '@mui/icons-material/ScienceOutlined';
 import WidgetsOutlinedIcon from '@mui/icons-material/WidgetsOutlined';
 import MCarausel from '~/components/Carausel/Medicine_carausel';
 import PlaylistAddOutlinedIcon from '@mui/icons-material/PlaylistAddOutlined';
+import { addMedicine, getMedicine, queryByFirstChar, queryMedByName, queryMedBCatOrAct} from '~/firebase';
+import { uploadImage } from '~/firebase';
 
 function Medicine() {
+    const initialState = {
+        number: "",
+        name: "",
+        catelogue: [],
+        type: "",
+        packing: "",
+        expiry: "",
+        active_element: [],
+        produce: "",
+        price: "",
+        unit: "",
+        quantity: 0,
+        img_url: "",
+        win_bid: []
+    };
+    
     const [obFilter, setOjFilter] = useState("tên thuốc");
-    const [data, setData] = useState(Medicines);
+    const [data, setData] = useState([]);
     const [searchItem, setSearchItem] = useState('');
-    function handleChange(event){
+    const [addItem, setAddItem] =useState(initialState);
+    useEffect(() => {
+        getMedicine((documents) => {
+            setData(documents);
+        });
+    }, []);
+
+    function changeSearch(event){
         const id = event.target.id;
         if ( id === "name"){
             setOjFilter("tên thuốc");
@@ -25,52 +49,69 @@ function Medicine() {
         }
     }
     function searchByFirstChar(event){
-        setData(Medicines.filter((item) => {
-            return (item.name[0] === event.target.id);
-        }
-        ))
+        queryByFirstChar( event.target.id, (documents) => {
+            setData(documents);
+        })
     }
     function checkData() {
         if (data.length === 0) {
-
           return <div class="d-flex justify-content-center p-5"><h1>Không tìm thấy thông tin...</h1></div>;
         }
         return <MCarausel medicines={data} />;
     }
-    function handleInputChange(event){
+    function changeInput(event){
         setSearchItem(event.target.value);
     }
-    function handleSubmit(event){
+    function submitSearch(event){
         event.preventDefault();
         if ( obFilter === "tên thuốc"){
-            setData(Medicines.filter((item) => {
-                return (item.name.toLowerCase().includes(searchItem.toLowerCase()));
-            }))
-        }
-        else if ( obFilter === "hoạt chất"){
-            setData(Medicines.filter((item) => {
-                return item.active_element.some(element => 
-                  element.toLowerCase().includes(searchItem.toLowerCase())
-                );
-            }));
+            queryMedByName( searchItem, (medicines) => {
+                setData(medicines);
+            })
         }
         else if ( obFilter === "danh mục"){
-            setData(Medicines.filter((item) => {
-                return item.catelogue.some( element => 
-                  element.toLowerCase().includes(searchItem.toLowerCase())
-                );
+            queryMedBCatOrAct( searchItem, "catelogue", (medicines) => {
+                setData(medicines);
+            })
+        }
+        else if ( obFilter === "hoạt chất"){
+            queryMedBCatOrAct( searchItem, "active_element", (medicines) => {
+                setData(medicines);
+            })
+        }
+    }
+    function changeSaveItem(event) {
+        const { name, value, files } = event.target;
+    
+        if (name === "img_url" && files[0]) {
+            uploadImage(files[0], (downloadURL) => {
+                setAddItem(prevState => ({
+                    ...prevState,
+                    [name]: downloadURL
+                }));
+            });
+        } else {
+            setAddItem(prevState => ({
+                ...prevState,
+                [name]: name === "catelogue" || name === "active_element" ? value.split(",").map(item => item.trim()) : name === "price" || name === "quantity" ? Number(value) : value
             }));
         }
+    }
+    
+    function submitAdd(event){
+        addMedicine(addItem).then(() => {
+            setAddItem(initialState);
+        })
     }
 
     return (
-        <div style={{width: "95vw", margin: "auto"}}>
+        <div>
             <div className={clsx(styles.searchWrapper)}> 
                 <div class="container py-md-5"> 
                     <div  class="row justify-content-center"> 
                         <div class="col-md-8"> 
                             <div class="search-content"> 
-                                <div class="jusbtn-toolbar d-flex flex-wrap justify-content-center" role="toolbar" aria-label="Toolbar with button groups">
+                                <div className={"jusbtn-toolbar d-flex flex-wrap justify-content-center"} role="toolbar" aria-label="Toolbar with button groups">
                                     <button   type="button" class="btn btn-outline-info" id="A" onClick={searchByFirstChar}> A </button>
                                     <button   type="button" class="btn btn-outline-info" id="B" onClick={searchByFirstChar}> B </button>
                                     <button   type="button" class="btn btn-outline-info" id="C" onClick={searchByFirstChar}> C </button>
@@ -99,24 +140,24 @@ function Medicine() {
                                     <button   type="button" class="btn btn-outline-info" id="Z" onClick={searchByFirstChar}> Z </button>
                                 </div>
                                 
-                                <form class="ng-untouched ng-pristine ng-valid" padding="5px" role="search" onSubmit={handleSubmit}> 
-                                    <div class="input-group mb-3" padding="5px">  
-                                        <input class="form-control w-50" type="search" placeholder={`Tìm kiếm theo ${obFilter}`} aria-label="Search" value={searchItem} onChange={handleInputChange}/>
-                                        <button class="btn btn-info" type="submit"> 
+                                <form className={"ng-untouched ng-pristine ng-valid"} padding="5px" role="search" onSubmit={submitSearch}> 
+                                    <div className={"input-group mb-3"} padding="5px">  
+                                        <input className={"form-control w-50"} type="search" placeholder={`Tìm kiếm theo ${obFilter}`} aria-label="Search" value={searchItem} onChange={changeInput}/>
+                                        <button className={"btn btn-info"} type="submit"> 
                                             <SearchIcon />
                                         </button>  
                                     </div> 
                                 </form>
-                                <div class="d-flex justify-content-center">
-                                    <div class="btn-group btn-group-lg" role="group" aria-label="Basic radio toggle button group">
-                                        <input type="radio" class="btn-check" name="btnradio" id="name" autocomplete="off" defaultChecked onChange={handleChange}/>
-                                        <label class="btn btn-outline-light" for="name"><MedicationOutlinedIcon/> Tên thuốc</label>
+                                <div className={"d-flex justify-content-center"}>
+                                    <div className={"btn-group btn-group-lg"} role="group" aria-label="Basic radio toggle button group">
+                                        <input type="radio" className={"btn-check"} name="btnradio" id="name" autocomplete="off" defaultChecked onChange={changeSearch}/>
+                                        <label className={"btn btn-outline-light"} for="name"><MedicationOutlinedIcon/> Tên thuốc</label>
 
-                                        <input type="radio" class="btn-check" name="btnradio" id="active_element" autocomplete="off" onChange={handleChange}/>
-                                        <label class="btn btn-outline-light" for="active_element"><ScienceOutlinedIcon/> Hoạt chất</label>
+                                        <input type="radio" className={"btn-check"} name="btnradio" id="active_element" autocomplete="off" onChange={changeSearch}/>
+                                        <label className={"btn btn-outline-light"} for="active_element"><ScienceOutlinedIcon/> Hoạt chất</label>
 
-                                        <input type="radio" class="btn-check" name="btnradio" id="catalogue" autocomplete="off" onChange={handleChange}/>
-                                        <label class="btn btn-outline-light" for="catalogue"><WidgetsOutlinedIcon/>Danh mục</label>
+                                        <input type="radio" className={"btn-check"} name="btnradio" id="catalogue" autocomplete="off" onChange={changeSearch}/>
+                                        <label className={"btn btn-outline-light"} for="catalogue"><WidgetsOutlinedIcon/>Danh mục</label>
                                     </div>
                                 </div>
                             </div>
@@ -125,8 +166,39 @@ function Medicine() {
                 </div> 
             </div>
             {checkData()}
-            <div class="d-grid col-3 mx-auto pb-5">
-                <button class="btn btn-info btn-lg" type="button"><h4><PlaylistAddOutlinedIcon/> Thêm</h4></button>
+            <div className={"d-grid col-3 mx-auto pb-5"}>
+                <button className={"btn btn-info btn-lg"} type="button"  data-bs-toggle="modal" data-bs-target="#addMedModal"><h4><PlaylistAddOutlinedIcon/> Thêm</h4></button>
+            </div>
+            <div className={"modal fade"} id="addMedModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className={"modal-dialog"}>
+                    <div className={"modal-content"}>
+                    <div className={"modal-header"}>
+                        <h5 className={"modal-title"} id="exampleModalLabel">Thêm thuốc</h5>
+                        <button type="button" className={"btn-close"} data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className={"modal-body"}>
+                        <input className={"form-control m-2"} type="text" name="name" placeholder="Tên thuốc" value={addItem.name} onChange={changeSaveItem}/>
+                        <input className={"form-control m-2"} type="text" name="number" placeholder="Số đăng kí" value={addItem.number} onChange={changeSaveItem}/>
+                        <input className={"form-control m-2"} type="text" name="catelogue" placeholder="Danh mục" value={addItem.catelogue} onChange={changeSaveItem}/>
+                        <input className={"form-control m-2"} type="text" name="type" placeholder="Dạng bào chế" value={addItem.type} onChange={changeSaveItem}/>
+                        <input className={"form-control m-2"} type="text" name="packing" placeholder="Đóng gói" value={addItem.packing} onChange={changeSaveItem}/>
+                        <input className={"form-control m-2"} type="text" name="expiry" placeholder="Hạng sử dụng" value={addItem.expiry} onChange={changeSaveItem}/>
+                        <input className={"form-control m-2"} type="text" name="active_element" placeholder="Hoạt chất" value={addItem.active_element} onChange={changeSaveItem}/>
+                        <input className={"form-control m-2"} type="text" name="produce" placeholder="Công ty sản xuất" value={addItem.produce} onChange={changeSaveItem}/>
+                        <input className={"form-control m-2"} type="file" name="img_url" placeholder="Link ảnh" onChange={changeSaveItem}/>
+                        <div class="input-group m-2">
+                            <span className={"input-group-text"} id="basic-addon1">đ</span>
+                            <input type="number" className={"form-control"} name="price" placeholder="Giá bán" value={addItem.price} onChange={changeSaveItem}/>
+                            <span className={"input-group-text"} id="basic-addon2">/</span>
+                            <input type="text" className={"form-control"} name="unit" placeholder="Đơn vị" value={addItem.unit} onChange={changeSaveItem}/>
+                        </div>
+                    </div>
+                    <div className={"modal-footer"}>
+                        <button type="button" className={"btn btn-secondary"} data-bs-dismiss="modal">Hủy</button>
+                        <button type="button" className={"btn btn-primary"} data-bs-dismiss="modal" onClick={submitAdd}>Lưu</button>
+                    </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
