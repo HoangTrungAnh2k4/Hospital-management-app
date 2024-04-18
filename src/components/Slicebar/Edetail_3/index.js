4e1c0cfbcdf96962c027dd0cb4f3c766b6787064
NGUYENKHANH-UT <120827290+NGUYENKHANH-UT@users.noreply.github.com>
2024-04-19 00:18:53 +0700

    Delete src/components/Slicebar directory

--- src/components/Slicebar/Edetail_3/index.js
+++
@@ -1,9 +0,0 @@
-function Edetail_3(props){
-    function handleClick(event){
-        props.setTarget(
-            [props.parentName, props.catalogues.name]
-        )
-    }
-    return <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded" onClick={handleClick}>{props.catalogues.name}</a></li>
-}
-export default Edetail_3;
