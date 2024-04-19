7eac937a0995aae6162c16befc1713115ca660a7
ThanhKhanh <nguyenthanhkhanh2304@gmail.com>
2024-04-19 17:43:30 +0700

    change size of add modal

--- src/pages/Equipment/index.js
+++ src/pages/Equipment/index.js
@@ -178,7 +178,7 @@ function Equipment() {
             </div>
             {checkData()}
             <div className={"modal fade"} id="addEquipModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className={"modal-dialog"}>
+                <div className={"modal-dialog modal-lg"}>
                     <div className={"modal-content"}>
                     <div className={"modal-header"}>
                         <h5 className={"modal-title"} id="exampleModalLabel">Thêm vật tư</h5>
